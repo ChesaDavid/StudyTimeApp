9a80b915ca910875679d534608d7db1d79b70630
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-07-08 15:20:17 +0300

    fresh start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,12 +26,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
