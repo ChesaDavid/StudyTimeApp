97937ec87873a04bab4c17a43ea23ba3b76e15e3
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-07-09 16:12:42 +0300

    MainActivity update and new and future parts of the projects start

--- app/src/main/java/com/example/studytimerappcode/MainActivity.java
+++ app/src/main/java/com/example/studytimerappcode/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.studytimerappcode;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +25,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        Button login = findViewById(R.id.login);
+        Button signup = findViewById(R.id.signup);
+        Button guest = findViewById(R.id.guest);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LogIn.class));
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+        
+    }
+
+
+
 }
