2bc63b8e93cb542f5796748355806c4d042280ab
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-07-11 15:59:54 +0300

    login page created

--- app/src/main/java/com/example/studytimerappcode/LogIn.java
+++ app/src/main/java/com/example/studytimerappcode/LogIn.java
@@ -1,6 +1,11 @@
 package com.example.studytimerappcode;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LogIn extends AppCompatActivity {
-
+    private Button login;
+    private EditText password,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,24 @@ public class LogIn extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        login = findViewById(R.id.loginbtn);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passwordField = password.getText().toString();
+                String emailField = email.getText().toString();
+                if(TextUtils.isEmpty(passwordField) || TextUtils.isEmpty(emailField)){
+                    Toast.makeText(LogIn.this,"All field are requierd.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    loginUser(passwordField,emailField);
+                }
+            }
+        });
+    }
+    private void loginUser(String email,String password){
+//        need to make the login function
     }
 }
