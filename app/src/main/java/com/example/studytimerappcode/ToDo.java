615585a95e24f3c0bd59c6adf1dc69ebc1d0919b
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-08-12 21:14:15 +0300

    this is the new way

---
+++ app/src/main/java/com/example/studytimerappcode/ToDo.java
@@ -0,0 +1,37 @@
+package com.example.studytimerappcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ToDo extends AppCompatActivity {
+
+    private ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_to_do);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+//        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ToDo.this,Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
